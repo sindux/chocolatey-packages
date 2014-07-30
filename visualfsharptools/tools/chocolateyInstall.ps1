8996211c7b0ca41eed809702348c1ff85081537e
Sindunata Sudarmaji <sindux@gmail.com>
2014-07-30 23:22:14 +0900

    Create Visual F# Tools package

---
+++ visualfsharptools/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'visualfsharptools' # arbitrary name for the package, used in messages
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/3/0/A/30A5D6DD-5B5C-4E06-B331-A88AA0B53150/FSharp_Bundle.exe'
+$silentArgs = '/install /quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
+
