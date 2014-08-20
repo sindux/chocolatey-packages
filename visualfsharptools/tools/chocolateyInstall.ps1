a98450ac2754f2aa220991dd4f949422fba7cd2a
Sindunata Sudarmaji <sindux@gmail.com>
2014-08-21 08:29:58 +0900

    Upgrade Visual F# Tools to 3.1.2

--- visualfsharptools/tools/chocolateyInstall.ps1
+++ visualfsharptools/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = 'visualfsharptools' # arbitrary name for the package, used in messages
+﻿$packageName = 'visualfsharptools'
 $installerType = 'EXE'
-$url = 'http://download.microsoft.com/download/3/0/A/30A5D6DD-5B5C-4E06-B331-A88AA0B53150/FSharp_Bundle.exe'
+$url = 'http://download.microsoft.com/download/E/A/3/EA38D9B8-E00F-433F-AAB5-9CDA28BA5E7D/FSharp_Bundle.exe'
 $silentArgs = '/install /quiet'
 $validExitCodes = @(0)
 
