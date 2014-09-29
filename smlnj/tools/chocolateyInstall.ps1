ba796830caea256137e6cfd47d7d1bac484ef6a5
Sindunata Sudarmaji <sindux@gmail.com>
2014-09-30 00:21:16 +0900

    Create SML/NJ package

---
+++ smlnj/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'smlnj'
+$installerType = 'MSI'
+$url = 'http://smlnj.cs.uchicago.edu/dist/working/110.77/smlnj-110.77.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
