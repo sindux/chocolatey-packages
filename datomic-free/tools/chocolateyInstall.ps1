5cc01dd81639e2d27e9e377135dddf83a67e91b9
Sindunata Sudarmaji <sindux@gmail.com>
2014-11-13 22:55:39 +0900

    Create Datomic Free package

---
+++ datomic-free/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'datomic-free'
+$url = 'https://my.datomic.com/downloads/free/0.9.5067'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
