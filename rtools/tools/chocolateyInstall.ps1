d79c67054aa297e5b6fd34fb1080059aad8f1a70
Sindunata Sudarmaji <sindux@gmail.com>
2015-01-17 00:15:38 +0900

    RTools 3.2

---
+++ rtools/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'rtools'
+$installerType = 'EXE'
+$url = 'http://cran.r-project.org/bin/windows/Rtools/Rtools32.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPath 'c:\Rtools\bin;c:\Rtools\gcc-4.6.3\bin' 'Machine'
