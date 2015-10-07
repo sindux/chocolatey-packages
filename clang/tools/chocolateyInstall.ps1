2cf5447d90a7aff9427c298bfff72ba4b259790f
Sindunata Sudarmaji <sindux@gmail.com>
2015-10-07 21:52:50 +0900

    Clang 3.7.0

--- clang/tools/chocolateyInstall.ps1
+++ clang/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 $packageName = 'clang'
 $installerType = 'EXE'
-$url = 'http://llvm.org/releases/3.6.0/LLVM-3.6.0-win32.exe'
+$ver='3.7.0'
+$url64 = "http://llvm.org/releases/$ver/LLVM-$ver-win64.exe"
+$url32 = "http://llvm.org/releases/$ver/LLVM-$ver-win32.exe"
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
-Install-ChocolateyPath "${env:ProgramFiles(x86)}\LLVM\bin"
-
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64" -validExitCodes $validExitCodes
+Install-ChocolateyPath "${env:ProgramFiles}\LLVM\bin"
