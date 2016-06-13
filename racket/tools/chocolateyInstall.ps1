17fa49bd677bb522e69b5c90c1a866d9b57d3209
Sindunata Sudarmaji <sindux@gmail.com>
2016-06-13 10:52:30 +0900

    Racket 6.5 (Apr 2016)

--- racket/tools/chocolateyInstall.ps1
+++ racket/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'racket'
 $installerType = 'EXE'
-$url = 'https://mirror.racket-lang.org/installers/6.4/racket-6.4-i386-win32.exe'
-$url64 = 'https://mirror.racket-lang.org/installers/6.4/racket-6.4-x86_64-win32.exe'
+$url = 'https://mirror.racket-lang.org/installers/6.5/racket-6.5-i386-win32.exe'
+$url64 = 'https://mirror.racket-lang.org/installers/6.5/racket-6.5-x86_64-win32.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
